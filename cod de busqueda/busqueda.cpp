dd39f7e89355b5352ee8fe8fc21512eeb6656c4b
mariandreamv <mariandrea.mv15@gmail.com>
2017-02-24 23:14:06 -0430

    binary search algorithm

--- cod de busqueda/busqueda.cpp
+++ cod de busqueda/busqueda.cpp
@@ -11,15 +11,16 @@ bool busquedaSimple(int array[],int size, int key){
 
 
 bool BusquedaBinaria(int array[],int size, int key,int start,int end){
-	
+	/*
 	int middle=(start+end)/2;
-	
 	if(start<=end){
-	
-				if(key==array[middle])
-					
+		std::cout<<"key:"<<key<<std::endl;
+		std::cout<<"array:"<<array[middle]<<std::endl;
+					if(key==array[middle]){
+					//	std::cout<<"key:"<<key<<std::endl;
+					//	std::cout<<"array:"<<array[middle]<<std::endl;
 					return true;
-				
+					}
 					if(key>array[middle]){
 						start=middle+1;
 						BusquedaBinaria(array,size, key,start,end);
@@ -27,12 +28,52 @@ bool BusquedaBinaria(int array[],int size, int key,int start,int end){
 					if(key<array[middle]) {
 						end=middle-1;
 						BusquedaBinaria(array,size, key,start,end);		
-					}
-				
-	
+					}	
 	}else return false;
-	
+	*/
 }
+
+
+int find(int intArray[], int data,int size) {
+   int lowerBound = 0;
+   int upperBound = size -1;
+   int midPoint = -1;
+   int comparisons = 0;      
+   int index = -1;
+	
+   while(lowerBound <= upperBound) {
+      printf("Comparison %d\n" , (comparisons +1) );
+      printf("lowerBound : %d, intArray[%d] = %d\n",lowerBound,lowerBound,
+         intArray[lowerBound]);
+      printf("upperBound : %d, intArray[%d] = %d\n",upperBound,upperBound,
+         intArray[upperBound]);
+      comparisons++;
+		
+      // compute the mid point
+      // midPoint = (lowerBound + upperBound) / 2;
+      midPoint = lowerBound + (upperBound - lowerBound) / 2;	
+		
+      // data found
+      if(intArray[midPoint] == data) {
+         index = midPoint;
+         break;
+      } else {
+         // if data is larger 
+         if(intArray[midPoint] < data) {
+            // data is in upper half
+            lowerBound = midPoint + 1;
+         }
+         // data is smaller 
+         else {
+            // data is in lower half 
+            upperBound = midPoint -1;
+         }
+      }               
+   }
+   printf("Total comparisons made: %d" , comparisons);
+   return index;
+}
+
 
 
 void insertion2(int vector[],int size){
@@ -60,12 +101,16 @@ int main(){
 	int size=100;
 	srand (time(NULL));
 	int array[size];
-	int in = 1;
+	int in = 2;
 	for(int i=0;i<size;i++) array[i]=rand()%size+1;
+	//-----------hasta aqui llenado aleatorio----------------//
 	insertion2(array,size);
-	if (BusquedaBinaria(array, size,in,0,size-1))
-	std::cout<<"encontrado";
-	else std::cout<<"NO encontrado"<<std::endl;
+	
+	if (find(array, in,size)>=0){
+	std::cout<<"\n\nencontrado\n"<<std::endl;
+	}else {
+	std::cout<<"\n\nNO encontrado\n"<<std::endl;
+	}
 	show_me(array,size);
 	
 }
